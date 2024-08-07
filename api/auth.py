56d104a0b8e8c045c9f2b3d457fb436a667d61a6
ZachDubroc <zvd1993@gmail.com>
2024-08-07 12:50:56 -0500

    railway

--- api/auth.py
+++ api/auth.py
@@ -14,7 +14,7 @@ load_dotenv()
 print(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
